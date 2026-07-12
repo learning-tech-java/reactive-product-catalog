0ffca2f356b8a828d33806abcba7e43d664bf3de
Nikolai <ne360cosmos@gmail.com>
2026-07-12 03:14:57 +0300

    Создан проект

---
+++ src/test/java/com/example/reactiveproductcatalog/ReactiveProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactiveproductcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveProductCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
