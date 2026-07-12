b69450348c57f7f496dfd259bd889ef1697b740f
Nikolai <ne360cosmos@gmail.com>
2026-07-12 23:32:26 +0300

    Добавлен R2DBC

--- service/src/main/java/com/example/reactiveproductcatalog/entity/ProductEntity.java
+++ service/src/main/java/com/example/reactiveproductcatalog/entity/ProductEntity.java
@@ -1,16 +1,34 @@
 package com.example.reactiveproductcatalog.entity;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Data
+@Table("products")
 public class ProductEntity {
 
+    /**
+     * Идентификатор товара
+     */
+    @Id
+    @Column("id")
     private UUID id;
 
+    /**
+     * Наименование товара
+     */
+    @Column("name")
     private String name;
 
+    /**
+     * Цена товара
+     */
+    @Column("price")
     private BigDecimal price;
+
 }
